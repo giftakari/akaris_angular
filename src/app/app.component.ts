b56e6481701716e1d68794b94d149630fa75640c
giftakari <giftakari@yahoo.com>
2023-09-18 17:06:21 +1000

    remove bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,47 +10,13 @@ import { BrandService } from './api/services';
 export class AppComponent implements OnInit {
   title = 'akaris_angular';
 
-  params: CreateBrand$Params = {
-    body: {
-      "@type": "CatalogProductOfferingsRequestAir",
-      "maxNumberOfUpsellsToReturn": 4,
-      "contentSourceList": [
-        "GDS"
-      ],
-      "PassengerCriteria": [
-        {
-          "@type": "PassengerCriteria",
-          "number": 1,
-          "passengerTypeCode": "ADT"
-        }
-      ],
-      "SearchCriteriaFlight": [
-        {
-          "@type": "SearchCriteriaFlight",
-          "departureDate": "2023-10-17",
-          "From": {
-            "value": "DEN"
-          },
-          "To": {
-            "value": "PHX"
-          },
-          "DepartureTimeRange": {
-            "start": "11:00:00",
-            "end": "13:00:00"
-          }
-        }
-      ]
-
-
-    }
-
-  };
+  
 
   constructor(private brandService: BrandService) {
 
   }
 
   ngOnInit() {
-    this.brandService.createBrand(this.params)
+
   }
 }
